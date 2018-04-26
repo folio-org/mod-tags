a5a72f0ed1f9468121a760e56d6e3662266cdd59
Heikki Levanto <heikki@indexdata.dk>
2018-04-26 13:16:22 +0200

    Towards the first implementation. Can POST and get a list

--- src/main/java/org/folio/rest/impl/TagsResourceImpl.java
+++ src/main/java/org/folio/rest/impl/TagsResourceImpl.java
@@ -2,24 +2,149 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import static io.vertx.core.Future.succeededFuture;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import javax.ws.rs.core.Response;
+import org.apache.commons.io.IOUtils;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.annotations.Validate;
+import org.folio.rest.jaxrs.model.Errors;
 import org.folio.rest.jaxrs.model.Tag;
+import org.folio.rest.jaxrs.model.TagsCollection;
 import org.folio.rest.jaxrs.resource.TagsResource;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
+import org.folio.rest.persist.PgExceptionUtil;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.persist.cql.CQLWrapper;
+import org.folio.rest.tools.messages.Messages;
+import org.folio.rest.tools.utils.OutStream;
+import org.folio.rest.tools.utils.TenantTool;
+import org.folio.rest.tools.utils.ValidationHelper;
+import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
+import org.z3950.zing.cql.cql2pgjson.FieldException;
+import org.z3950.zing.cql.cql2pgjson.SchemaException;
+
+// TODO:
+// - Find and rename all mentions of notify (once all code is copied in)
+// - Enable CQL validation, latest RMB must support it
+
 
 
 public class TagsResourceImpl implements TagsResource {
+  private final Logger logger = LoggerFactory.getLogger("mod-tags");
+  private final Messages messages = Messages.getInstance();
+  private static final String NOTIFY_TABLE = "tags";
+  private static final String LOCATION_PREFIX = "/tags/";
+  private static final String IDFIELDNAME = "_id";
+  private String notifySchema = null;
+  private static final String NOTIFY_SCHEMA_NAME = "apidocs/raml/tags.json";
+
+  private void initCQLValidation() {
+    try {
+      notifySchema = IOUtils.toString(getClass().getClassLoader().getResourceAsStream(NOTIFY_SCHEMA_NAME), "UTF-8");
+    } catch (Exception e) {
+      logger.error("unable to load schema - " + NOTIFY_SCHEMA_NAME + ", validation of query fields will not be active");
+    }
+  }
+
+  public TagsResourceImpl(Vertx vertx, String tenantId) {
+    if (notifySchema == null) { // Commented out, the validation fails a
+      //initCQLValidation();   // prerfectly valid query=metaData.createdByUserId=e037b...
+    }
+    PostgresClient.getInstance(vertx, tenantId).setIdField(IDFIELDNAME);
+  }
+
+  private CQLWrapper getCQL(String query, int limit, int offset)
+    throws FieldException, IOException, SchemaException {
+    CQL2PgJSON cql2pgJson;
+    if (notifySchema != null) {
+      cql2pgJson = new CQL2PgJSON(NOTIFY_TABLE + ".jsonb", notifySchema);
+    } else {
+      cql2pgJson = new CQL2PgJSON(NOTIFY_TABLE + ".jsonb");
+    }
+    CQLWrapper wrap = new CQLWrapper(cql2pgJson, query);
+    if (limit >= 0) {
+      wrap.setLimit(new Limit(limit));
+    }
+    if (offset >= 0) {
+      wrap.setOffset(new Offset(offset));
+    }
+    return wrap;
+  }
+
 
   @Override
-  public void getTags(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  @Validate
+  public void getTags(String query,
+    int offset, int limit, String lang,
+    Map<String, String> okapiHeaders,
+    Handler<AsyncResult<Response>> asyncResultHandler,
+    Context vertxContext) throws Exception {
+
+    CQLWrapper cql;
+    String tenantId = TenantTool.calculateTenantId(
+      okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+    try {
+      logger.debug("Getting tags. "
+        + offset + "+" + limit + " q=" + query);
+      cql = getCQL(query, limit, offset);
+    } catch (Exception e) {
+      ValidationHelper.handleError(e, asyncResultHandler);
+      return;
+    }
+    PostgresClient.getInstance(vertxContext.owner(), tenantId)
+      .get(NOTIFY_TABLE, Tag.class, new String[]{"*"}, cql,
+        true /*get count too*/, false /* set id */,
+        reply -> {
+          if (reply.succeeded()) {
+            TagsCollection notes = new TagsCollection();
+            @SuppressWarnings("unchecked")
+            List<Tag> notifylist
+              = (List<Tag>) reply.result().getResults();
+            notes.setTags(notifylist);
+            Integer totalRecords = reply.result().getResultInfo().getTotalRecords();
+            notes.setTotalRecords(totalRecords);
+            asyncResultHandler.handle(succeededFuture(
+              GetTagsResponse.withJsonOK(notes)));
+          } else {
+            ValidationHelper.handleError(reply.cause(), asyncResultHandler);
+          }
+        });
   }
 
   @Override
-  public void postTags(String lang, Tag entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+  @Validate
+  public void postTags(String lang,
+      Tag entity,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
+      Context context) throws Exception {
+    
+      String tenantId = TenantTool.calculateTenantId(
+        okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+      String id = entity.getId();
+      PostgresClient.getInstance(context.owner(), tenantId).save(NOTIFY_TABLE,
+        id, entity,
+        reply -> {
+          if (reply.succeeded()) {
+            Object ret = reply.result();
+            entity.setId((String) ret);
+            OutStream stream = new OutStream();
+            stream.setData(entity);
+            asyncResultHandler.handle(succeededFuture(PostTagsResponse
+                .withJsonCreated(LOCATION_PREFIX + ret, stream)));
+          } else {
+            ValidationHelper.handleError(reply.cause(), asyncResultHandler);
+          }
+        });
+    }
 
   @Override
   public void getTagsById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
---
+++ src/test/java/org/folio/rest/impl/TagsTest.java
@@ -0,0 +1,157 @@
+package org.folio.rest.impl;
+
+import com.jayway.restassured.RestAssured;
+import static com.jayway.restassured.RestAssured.given;
+import com.jayway.restassured.response.Header;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.util.UUID;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.PomReader;
+import org.folio.rest.tools.client.test.HttpClientMock2;
+import static org.hamcrest.Matchers.containsString;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit tests for mod-tags.
+ * Tags is a simple module, so the test are pretty simple too. We run against
+ * the embedded postgres (oby default on port 9248, unlikely to collide with
+ * a real postgress, or anything else), so we always start from a clean slate,
+ * and there is no need to clean up afterwards.
+ */
+@RunWith(VertxUnitRunner.class)
+public class TagsTest {
+
+  private final Logger logger = LoggerFactory.getLogger("TagsTest");
+  private final int port = Integer.parseInt(System.getProperty("port", "8081"));
+  private final int pgPort = Integer.parseInt(System.getProperty("pgPort","9248"));
+  private static final String LS = System.lineSeparator();
+  private final Header TEN = new Header("X-Okapi-Tenant", "testlib");
+
+  private final Header JSON = new Header("Content-Type", "application/json");
+  private String moduleName; // "mod-tags";
+  private String moduleVersion; // "0.2.0-SNAPSHOT";
+  private String moduleId; // "mod-tags-0.2.0-SNAPSHOT"
+  Vertx vertx;
+  Async async;
+
+  @Before
+  public void setUp(TestContext context) {
+    vertx = Vertx.vertx();
+    moduleName = PomReader.INSTANCE.getModuleName()
+      .replaceAll("_", "-");  // Rmb returns a 'normalized' name, with underscores
+    moduleVersion = PomReader.INSTANCE.getVersion();
+    moduleId = moduleName + "-" + moduleVersion;
+
+    logger.info("Test setup starting for " + moduleId);
+
+    try {
+      PostgresClient.setIsEmbedded(true);
+      PostgresClient.setEmbeddedPort(pgPort);
+      PostgresClient.getInstance(vertx).startEmbeddedPostgres();
+    } catch (Exception e) {
+      context.fail(e);
+      return;
+    }
+
+    JsonObject conf = new JsonObject()
+      .put(HttpClientMock2.MOCK_MODE, "true")
+      .put("http.port", port);
+
+    logger.info("tagsTest: Deploying "
+      + RestVerticle.class.getName() + " "
+      + Json.encode(conf));
+    DeploymentOptions opt = new DeploymentOptions()
+      .setConfig(conf);
+    vertx.deployVerticle(RestVerticle.class.getName(),
+      opt, context.asyncAssertSuccess());
+    RestAssured.port = port;
+    logger.info("tagsTest: setup done. Using port " + port);
+  }
+
+  @After
+  public void tearDown(TestContext context) {
+    logger.info("Cleaning up after tagsTest");
+    async = context.async();
+    PostgresClient.stopEmbeddedPostgres();
+    vertx.close(res -> {   // This logs a stack trace, ignore it.
+      logger.info("tagsTest done");
+      async.complete();
+    });
+  }
+
+
+
+  @Test
+  public void tests(TestContext context) throws  Exception {
+    async = context.async();
+    logger.info("tagsTest starting ==== ");  // search for ==== in mvn output
+
+    UUID id1 = UUID.randomUUID();
+    
+    // Simple GET request to see the module is running and we can talk to it.
+    given()
+      .get("/admin/health")
+      .then().log().ifValidationFails()
+      .statusCode(200);
+
+    // Call the tenant interface to initialize the database
+    String tenants = "{\"module_to\":\"" + moduleId + "\"}";
+    logger.info("About to call the tenant interface " + tenants);
+    given()
+      .header(TEN).header(JSON)
+      .body(tenants)
+      .post("/_/tenant")
+      .then().log().ifValidationFails()
+      .statusCode(201);
+    logger.info("Tenant interface ok ====");
+
+    logger.info("Empty list of tags");
+    given()
+      .header(TEN)
+      .get("/tags")
+      .then().log().ifValidationFails()
+      .statusCode(200)
+      .body(containsString("\"tags\" : [ ]"));
+
+    logger.info("Post a tag");
+    String tag1 = "{\"id\" : \"" + id1 + "\", "
+            + "\"label\" : \"First tag\", "
+            + "\"description\" : \"This is the first test tag\" }";
+    logger.info("Post a tag");
+    given()
+      .header(TEN).header(JSON)
+      .body(tag1)
+      .post("/tags")
+      .then().log().ifValidationFails()
+      .statusCode(201)
+      .body(containsString("first test"));
+
+    logger.info("List of that one tag");
+    given()
+      .header(TEN)
+      .get("/tags")
+      .then().log().ifValidationFails()
+      .statusCode(200)
+      .body(containsString("the first test"))
+      .body(containsString("\"totalRecords\" : 1"));
+
+    // All done
+    logger.info("tagsTest done ==== ");
+    async.complete();
+
+  }
+
+
+}
