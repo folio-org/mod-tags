3745264bdd6c5bf2b3f79010796ea99212605f8d
Gurleen Kaur <gurleen_kaur1@epam.com>
2024-10-24 19:02:50 +0530

    MODTAG-126 - Upgrade folio spring support version.

--- src/test/java/org/folio/tags/support/ApiTest.java
+++ src/test/java/org/folio/tags/support/ApiTest.java
@@ -4,6 +4,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -43,7 +45,7 @@ public abstract class ApiTest {
   protected static final String USER_ID = "77777777-7777-7777-7777-777777777777";
 
   @Container
-  static PostgreSQLContainer<?> postgreDBContainer = new PostgreSQLContainer<>("postgres:16-alpine");
+  static PostgreSQLContainer<?> postgreDBContainer = Objects.toString(System.getenv("TESTCONTAINERS_POSTGRES_IMAGE"), "postgres:16-alpine"))
 
   static {
     postgreDBContainer.start();
